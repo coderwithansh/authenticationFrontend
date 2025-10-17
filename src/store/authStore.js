1199a0b91bbaa615eb7bd29085872fc46093797a
ansh <anshdewangna52@gmail.com>
2025-10-17 16:30:28 +0530

    enter the backend api in authstore

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,7 +1,7 @@
 import{create} from 'zustand';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://auth-mern-backend-five.vercel.app/api/auth';
 
 axios.defaults.withCredentials = true;
 
